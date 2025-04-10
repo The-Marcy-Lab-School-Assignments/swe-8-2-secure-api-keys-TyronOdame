2c66e187620bc16b3dc4c7385bde45f3c8e80c70
Tyron <tyronodame5@gmail.com>
2025-04-10 14:35:41 -0400

    fixed bug

--- frontend/src/adapters/giphyAdapters.js
+++ frontend/src/adapters/giphyAdapters.js
@@ -3,9 +3,9 @@ import { handleFetch } from './handleFetch.js';
 // Send a fetch request to the /trending endpoint and return the top 3 results
 export const getTrendingGifs = async () => {
   const url = '/api/gifs';
-  const [data, error] = await handleFetch(url);
+  // const [data, error] = await handleFetch(url);
 
-  return [data, error];
+  return await handleFetch(url);
 };
 
 export const getGifsBySearch = async (searchTerm) => {};
